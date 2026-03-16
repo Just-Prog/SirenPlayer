99f2b5e7966962f6e309b7a270ee0a65f7f18551
Just-Prog <JustProg5835@outlook.com>
2026-03-16 15:21:50 +0800

    init first screen

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function AppOpeningView() {
+  return (
+    <SafeAreaView>
+      <Text>test</Text>
+    </SafeAreaView>
+  );
+}
