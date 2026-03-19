b45be6a363d32496aa3b7474b82d037cbca93759
Just-Prog <JustProg5835@outlook.com>
2026-03-19 15:08:54 +0800

    fix:`metro.config.js` been f**ked by biome

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(import.meta.dirname);
+// biome-ignore lint/correctness/noGlobalDirnameFilename: 跟Windows的NodeJS ESM导入器说去吧
+const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: "./global.css" });
