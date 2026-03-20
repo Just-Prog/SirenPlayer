62f7abb052c5a92442d4c1752b8ef7181e23b975
Just-Prog <JustProg5835@outlook.com>
2026-03-20 16:27:22 +0800

    chore: idk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="home" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar />
+    </SafeAreaProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,22 @@
-import { Text } from "react-native";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AppOpeningView() {
+  const router = useRouter();
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/home");
+    }, 2000);
+  }, []);
   return (
     <SafeAreaView>
-      <Text>test</Text>
+      <View className="flex h-screen w-screen flex-col items-center justify-center gap-y-4">
+        <Text className="text-nowrap font-bold text-xl">Siren Player</Text>
+        <Text className="text-nowrap">@Monster Siren</Text>
+      </View>
     </SafeAreaView>
   );
 }
