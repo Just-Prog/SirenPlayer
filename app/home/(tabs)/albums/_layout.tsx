58c5eec908f9c2808e89847fc2221b5238188b2f
Just-Prog <JustProg5835@outlook.com>
2026-03-28 22:35:16 +0800

    feat: move player bar with tabs layout together

--- app/home/(tabs)/albums/_layout.tsx
+++ app/home/(tabs)/albums/_layout.tsx
@@ -1,18 +1,10 @@
 import { Stack } from "expo-router";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import useTrackPlayer from "@/hooks/useTrackPlayer";
 
 export default function HomeAlbumListLayout() {
-  const { Player } = useTrackPlayer();
-  const safetyzone = useSafeAreaInsets();
-
   return (
-    <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="detail" />
-      </Stack>
-      <Player bottomMargin={safetyzone.bottom} />
-    </>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="detail" />
+    </Stack>
   );
 }
--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,9 +1,17 @@
+import useTrackPlayer from "@/hooks/useTrackPlayer";
 import { Stack } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function HomeLayout() {
+  const { Player } = useTrackPlayer();
+  const safetyzone = useSafeAreaInsets();
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+      <Player bottomMargin={safetyzone.bottom + 54} />
+    </>
   );
 }
