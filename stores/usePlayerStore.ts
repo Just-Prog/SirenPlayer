006bcf3783d69cde4ee241a17cfce2ca56598f75
Just-Prog <JustProg5835@outlook.com>
2026-04-09 14:46:07 +0800

    feat: persist player state

--- stores/usePlayerStore.ts
+++ stores/usePlayerStore.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import type { AlbumsListItemProps } from "@/types/albums";
 
 interface PlayerStoreProps {
@@ -12,15 +14,23 @@ interface PlayerStoreActionProps {
   setPlaylist: (arg1: PlayerStoreProps["playlist"]) => void;
 }
 
-const usePlayerStore = create<PlayerStoreProps & PlayerStoreActionProps>()(
-  (set) => ({
-    current: 0,
-    playlist: [],
-    setCurrent: (arg1) => set(() => ({ current: arg1 })),
-    setPlaylist: (arg1) => set(() => ({ playlist: arg1 })),
-    replacePlaylist: (arg1, arg2) =>
-      set(() => ({ current: arg2, playlist: arg1 })),
-  })
+type PlayerStoreCombinedProps = PlayerStoreActionProps & PlayerStoreProps;
+
+const usePlayerStore = create<PlayerStoreCombinedProps>()(
+  persist(
+    (set) => ({
+      current: 0,
+      playlist: [],
+      setCurrent: (arg1) => set(() => ({ current: arg1 })),
+      setPlaylist: (arg1) => set(() => ({ playlist: arg1 })),
+      replacePlaylist: (arg1, arg2) =>
+        set(() => ({ current: arg2, playlist: arg1 })),
+    }),
+    {
+      name: "siren_state",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
 );
 
 export default usePlayerStore;
