63ba3e2d6e0c4443743b694fae699d36ea3cab27
Just-Prog <JustProg5835@outlook.com>
2026-04-16 15:55:09 +0800

    feat: opt album detail screen loading ui

--- components/albums/AlbumsDetailListView.tsx
+++ components/albums/AlbumsDetailListView.tsx
@@ -56,102 +56,110 @@ const AlbumsDetailListView = () => {
           />
         </TouchableOpacity>
       </View>
-      <View className="px-6">
-        <Text className="line-clamp-1 text-nowrap font-bold text-4xl">
-          {detail?.name ?? "专辑"}
-        </Text>
-        <Text className="mt-1 line-clamp-1 text-nowrap font-light text-lg">
-          {detail?.artistes
-            ? detail.artistes.join("、")
-            : "MonsterSirenRecords"}
-        </Text>
-      </View>
-      <View className="flex-row gap-x-4 px-6">
-        <Image
-          source={{
-            uri: detail?.coverUrl ?? "",
-          }}
-          style={{
-            height: window.width / 2 - 64,
-            width: window.width / 2 - 64,
-            marginTop: 12,
-            marginBottom: 12,
-            backgroundColor: "#e0e0e0",
-            borderRadius: 16,
-            shadowColor: "#000000",
-            shadowRadius: 2,
-            shadowOpacity: 0.6,
-          }}
-        />
-        <View className="flex-1 flex-col gap-y-2">
-          <ScrollView style={{ width: "100%", height: window.width / 2 - 64 }}>
-            <Text>{detail?.intro}</Text>
-          </ScrollView>
-          <MaterialIcons
-            className="absolute -top-16 -right-4 -z-10"
-            color={"#eee0e0e0"}
-            name={"format-quote"}
-            size={window.width / 4}
-          />
-        </View>
-      </View>
-      <View className="flex-1">
-        {isLoading ? (
-          <View
-            style={{
-              marginBottom:
-                (16 + safetyzone.bottom) * 2 +
-                (Platform.OS === "ios" ? safetyzone.bottom : 0) +
-                96,
-            }}
-          >
-            <LoadingIcon loading={isLoading} />
+      {isLoading ? (
+        <LoadingIcon loading={isLoading} />
+      ) : (
+        <>
+          <View className="px-6">
+            <Text className="line-clamp-1 text-nowrap font-bold text-4xl">
+              {detail?.name ?? "专辑"}
+            </Text>
+            <Text className="mt-1 line-clamp-1 text-nowrap font-light text-lg">
+              {detail?.artistes
+                ? detail.artistes.join("、")
+                : "MonsterSirenRecords"}
+            </Text>
           </View>
-        ) : (
-          <FlatList
-            className={""}
-            data={detail?.songs ?? []}
-            ItemSeparatorComponent={() => {
-              return (
-                <View className="my-2 flex w-screen flex-1 flex-col items-center justify-center">
-                  <View
-                    className="flex-1 border border-gray-200/65"
-                    style={{ width: "95%" }}
-                  />
-                </View>
-              );
-            }}
-            ListFooterComponent={() => (
+          <View className="flex-row gap-x-4 px-6">
+            <Image
+              source={{
+                uri: detail?.coverUrl ?? "",
+              }}
+              style={{
+                height: window.width / 2 - 64,
+                width: window.width / 2 - 64,
+                marginTop: 12,
+                marginBottom: 12,
+                backgroundColor: "#e0e0e0",
+                borderRadius: 16,
+                shadowColor: "#000000",
+                shadowRadius: 2,
+                shadowOpacity: 0.6,
+              }}
+            />
+            <View className="flex-1 flex-col gap-y-2">
+              <ScrollView
+                style={{ width: "100%", height: window.width / 2 - 64 }}
+              >
+                <Text>{detail?.intro}</Text>
+              </ScrollView>
+              <MaterialIcons
+                className="absolute -top-16 -right-4 -z-10"
+                color={"#eee0e0e0"}
+                name={"format-quote"}
+                size={window.width / 4}
+              />
+            </View>
+          </View>
+          <View className="flex-1">
+            {isLoading ? (
               <View
                 style={{
-                  height:
+                  marginBottom:
                     (16 + safetyzone.bottom) * 2 +
-                    (Platform.OS === "ios" ? safetyzone.bottom * 2.5 : 0) +
-                    72,
+                    (Platform.OS === "ios" ? safetyzone.bottom : 0) +
+                    96,
                 }}
+              >
+                <LoadingIcon loading={isLoading} />
+              </View>
+            ) : (
+              <FlatList
+                className={""}
+                data={detail?.songs ?? []}
+                ItemSeparatorComponent={() => {
+                  return (
+                    <View className="my-2 flex w-screen flex-1 flex-col items-center justify-center">
+                      <View
+                        className="flex-1 border border-gray-200/65"
+                        style={{ width: "95%" }}
+                      />
+                    </View>
+                  );
+                }}
+                ListFooterComponent={() => (
+                  <View
+                    style={{
+                      height:
+                        (16 + safetyzone.bottom) * 2 +
+                        (Platform.OS === "ios" ? safetyzone.bottom * 2.5 : 0) +
+                        72,
+                    }}
+                  />
+                )}
+                renderItem={({ item, index }) => (
+                  <Pressable
+                    onPress={() =>
+                      replacePlaylist(
+                        detail?.songs.map((song) => ({
+                          ...song,
+                          coverUrl: detail?.coverUrl,
+                        })),
+                        index
+                      )
+                    }
+                  >
+                    <View className="my-2 w-full flex-row items-center gap-x-2 px-6">
+                      <Ionicons name="play" size={16} />
+                      <Text className="font-bold">{item.name}</Text>
+                    </View>
+                  </Pressable>
+                )}
               />
             )}
-            renderItem={({ item, index }) => (
-              <Pressable
-                onPress={() =>
-                  replacePlaylist(
-                    detail?.songs.map((song) => ({
-                      ...song,
-                      coverUrl: detail?.coverUrl,
-                    })),
-                    index
-                  )
-                }
-              >
-                <View className="my-2 w-full flex-row items-center gap-x-2 px-6">
-                  <Ionicons name="play" size={16} />
-                  <Text className="font-bold">{item.name}</Text>
-                </View>
-              </Pressable>
-            )}
-          />
-        )}
-      </View>
+          </View>
+        </>
+      )}
     </View>
   );
 };
